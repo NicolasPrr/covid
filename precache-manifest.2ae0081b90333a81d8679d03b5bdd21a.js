c17bb73700a2a3f20cb280415d017d38f26902a7
nicolasprr <niparrara@unal.edu.co>
2020-03-23 21:04:11 -0500

    Updates

---
+++ precache-manifest.2ae0081b90333a81d8679d03b5bdd21a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0398a32372f64c99ff8a12bed1fee6ff",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "eb58bcb938ce3a425c3f",
+    "url": "/covid/static/css/2.51d27c34.chunk.css"
+  },
+  {
+    "revision": "eb58bcb938ce3a425c3f",
+    "url": "/covid/static/js/2.6d37eece.chunk.js"
+  },
+  {
+    "revision": "1e9ac90d1b3229c3e27e20569e5be6a1",
+    "url": "/covid/static/js/2.6d37eece.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77ba4fc3adff0ff38c7f",
+    "url": "/covid/static/js/main.6c2142fc.chunk.js"
+  },
+  {
+    "revision": "c3609e11e6a69f649fc5",
+    "url": "/covid/static/js/runtime-main.b6f74cb8.js"
+  }
+]);
